af598754d049cf62baa25747f4668be45103f4ab
logancrow <43042565+logancrow@users.noreply.github.com>
2019-02-26 19:05:17 -0600

    Add files via upload
    wrote code in all modules except clkdiv1s and clkdiv128hz that compiles
    work still to be completed: write last mode case statement, write 2 clock dividers, debug, fill out constraints file

--- fitbit.v
+++ fitbit.v
@@ -28,12 +28,14 @@ module fitbit(
     );
     
     wire clk1s;
+    wire clk128hz;
     
     clkdiv1s c0 (clk,clk1s);
+    clkdiv128hz c1 (clk,clk128hz);
     
     wire pulse;
     
-    pulsegenerator p0 (mode,clk,reset,start,pulse);
+    pulsegenerator p0 (mode,clk,clk128hz,clk1s,reset,start,pulse);
     
     wire [20:0] steps;
     wire [13:0] saturated;
@@ -46,11 +48,11 @@ module fitbit(
     
     wire [3:0] secondsover32;
     
-    stepsover32 s1 (steps,clk1s,reset,secondsover32);
+    stepsover32 s1 (steps,clk1s,reset,start,secondsover32);
     
     wire [13:0] highactsecs;
     
-    highactivity a0 (steps,clk1s,reset,highactsecs);
+    highactivity a0 (steps,clk1s,reset,start,highactsecs);
     
     wire binout;
     
@@ -128,18 +130,60 @@ endmodule
 //generates pulse to send to step counter based on mode
 module pulsegenerator(
     input [1:0] mode,
-    input clk, reset, start,
-    output pulse
+    input clk, clk128hz, clk1s, reset, start,
+    output reg pulse
     );
+    
+    wire clk64hz, clk32hz;
+    
+    clkdiv2 c2 (clk128hz,clk64hz);
+    clkdiv2 c3 (clk64hz,clk32hz);
+    
+    reg off;
+    
+    initial begin
+        off = 1'b1;
+        pulse = 1'b0;
+    end
+    
+    always@(posedge reset) off <= 1'b1;
+    always@(posedge start) off <= 1'b0;
+    
+    always@(*) begin
+        case(mode)
+            2'b00 : begin if(!off) pulse <= clk32hz; else pulse = 1'b0; end
+            2'b01 : begin if(!off) pulse <= clk64hz; else pulse = 1'b0; end
+            2'b10 : begin if(!off) pulse <= clk128hz; else pulse = 1'b0; end
+            //need to write code for case 4
+        endcase
+        
+    end
   
 endmodule
 
 //module that takes input from the pulse generator and outputs step count and saturated step count
 module stepcounter(
     input pulse, reset,
-    output [20:0] steps,
-    output [13:0] saturated
+    output reg [20:0] steps,
+    output reg [13:0] saturated
     );
+    
+    initial begin
+        steps = 0;
+        saturated = 0;
+    end
+    
+    always@(posedge reset) begin 
+        steps <= 0; 
+        saturated <= 0;
+    end
+    
+    always@(posedge pulse) begin
+        steps <= steps + 1;
+        if(steps > 9999) 
+             saturated <= 9999; 
+             else saturated <= steps; 
+    end
 
 endmodule
 
@@ -149,23 +193,95 @@ module distancecovered(
     output [13:0] distance
     );
     
+    reg [2:0] decimal;
+    
+    initial decimal = 0;
+    
+    assign distance = (10*(steps/2048)) | decimal;
+    
+    always@(*) begin
+        if((steps % 2048) > 1024) decimal <= 5;
+            else decimal <= 0;
+    end
 endmodule
 
 //calculates how many of the first 9 seconds had over 32 steps/sec
 module stepsover32(
     input [20:0] steps,
-    input clk, reset,
-    output [3:0] seconds
+    input clk, reset, start,
+    output reg [3:0] seconds
     );
+    
+    reg [3:0] counter;
+    reg [20:0] prev_steps;
+    reg off;
+    
+    initial begin 
+        counter = 0;
+        prev_steps = 0;
+        seconds = 0;
+        off = 1'b1;
+    end
+    
+    always@(posedge reset) begin
+        off <= 1'b1;
+        counter <= 0;
+        prev_steps <= 0;
+        seconds <= 0;
+    end
+    
+    always@(posedge start) off <= 1'b0;
+    
+    always@(posedge clk) begin
+        if(!off) begin
+            if(counter < 9) begin
+                counter <= counter + 1;
+                if((steps - prev_steps) > 32) seconds <= seconds + 1;
+            end
+            prev_steps <= steps;
+        end
+    end
     
 endmodule
 
 //calculates time of high activity (seconds over 64 steps/sec after 1 consecutive minute over 64)
 module highactivity(
     input [20:0] steps,
-    input clk, reset,
-    output [13:0] seconds   
+    input clk, reset, start,
+    output reg [13:0] seconds   
     );
+    
+    reg off;
+    reg [13:0] counter;
+    reg [20:0] prev_steps;
+    
+    initial begin
+        off = 1'b1;
+        counter = 0;
+        prev_steps = 0;
+    end
+    
+    always@(posedge reset) begin
+        off <= 1'b1;
+        counter <= 0;
+        seconds <= 0;
+        prev_steps <= 0;
+    end
+    
+    always@(posedge start) begin
+        off <= 1'b0;
+    end
+    
+    always@(posedge clk) begin
+        if(!off) begin
+            if((steps - prev_steps) > 64) begin
+                counter = counter + 1;
+                if(counter == 60) seconds  <= seconds + 60;
+                if(counter > 60) seconds <= seconds + 1;
+            end
+            prev_steps <= steps;
+        end
+    end
     
 endmodule
 
@@ -174,17 +290,40 @@ module modulecycler(
     input [13:0] steps, distance, highactivity,
     input [3:0] over32,
     input clk,
-    output [13:0] out, 
-    output sat, dp
+    output reg [13:0] out, 
+    output reg sat, dp
     );
+    
+    reg [2:0] counter;
+    
+    initial begin
+        counter  = 0;
+    end
+    
+    always@(*) begin
+        case({counter[2],counter[1]})
+            2'b00 : begin out <= steps; if(steps == 9999) sat <= 1'b1;  else sat <= 1'b0; dp <= 1'b0; end
+            2'b01 : begin out <= distance; dp <= 1'b1; sat <= 1'b0; end
+            2'b10 : begin out <= over32; dp <= 1'b0; sat <= 1'b0; end
+            2'b11 : begin out <= highactivity; dp <= 1'b0; sat <= 1'b0; end         
+        endcase
+    end
+    
+    always@(posedge clk) counter = (counter + 1)%8;
     
 endmodule
 
 //converts binary number to 4 seperate digits
 module binconverter(
     input [13:0] in,
-    output [3:0] out3, out2, out1, out0
+    output [3:0] out3, 
+    inout [3:0] out2, out1, out0
     );
+    
+    assign out0 = in%10;
+    assign out1 = (in%100) - out0;
+    assign out2 = (in%1000) - out1 - out0;
+    assign out3 = in - out2 - out1 - out0;
     
 endmodule
 
@@ -194,4 +333,23 @@ module clkdiv1s(
     output clk_out
     );
     
+endmodule
+
+//divides clock to 128 Hz
+module clkdiv128hz(
+    input clk,
+    output clk_out
+    );
+    
+endmodule
+
+//output clock is half the frequency of the input clock
+module clkdiv2(
+    input clk,
+    output reg clk_out
+    );
+    initial clk_out = 0;
+    
+    always@(posedge clk) clk_out = !clk_out;
+
 endmodule
